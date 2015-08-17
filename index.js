7058e1166c95e8fee0d08aaced1ae1a987be3694
John Hatcher <jhatcher@jibe.com>
2015-08-16 23:23:45 -0400

    finish most the refactoring

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var server  = require('./lib/server');
+let cluster = require('cluster');
+let numCPUs = require('os').cpus().length;
+let server  = require('./lib/server');
 
 // but before we listen...fork the process so that incoming connections
 // are balanced (round-robin) over each available CPU core.
 if (cluster.isMaster) {
 
-  for (var i = 0; i < numCPUs; i++) {
+  cluster.on('online', worker => {
+    console.log('worker %s came online', worker.process.pid);
+  });
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error('worker %s died by %s', worker.process.pid, signal || code);
+    cluster.fork(); // start up a new one
+  });
+
+  for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
-  cluster.on('exit', function (worker, code, signal) {
-    console.error('worker ' + worker.process.pid + ' died!');
-
-    // TODO: handle the error...
-
-  });
 
 } else {
   server.run(); // workers share the master socket
