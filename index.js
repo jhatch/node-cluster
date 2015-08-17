d7e05cb7d8ee22d8affc011e770f6e7a9f7fad6a
John Hatcher <jhatcher@jibe.com>
2015-08-16 22:27:40 -0400

    refactoring to separate the server definition from the cluster forking

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-var express = require('express');
-var config  = require('config');
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
-
-// as we are trying to demonstrate cross process communication
-// make a simple utility logger showing PID
-function pLog() {
-  var args =  Array.prototype.slice.call(arguments);
-  args.unshift('[PID=' + process.pid + ']');
-  console.log.apply(console, args);
-}
-
-// now define our web application itself
-var app = express();
-
-app.get('/test', function (req, res) {
-  pLog('handling a request');
-  return res.send('cluster fudge');
-});
+var server  = require('./lib/server');
 
 // but before we listen...fork the process so that incoming connections
 // are balanced (round-robin) over each available CPU core.
@@ -28,14 +11,12 @@ if (cluster.isMaster) {
   }
 
   cluster.on('exit', function (worker, code, signal) {
-    pLog('worker ' + worker.process.pid + ' died');
+    console.error('worker ' + worker.process.pid + ' died!');
+
+    // TODO: handle the error...
+
   });
 
 } else {
-
-  // workers share the master socket
-  app.listen(config.server.port, null, null, function () {
-    pLog('server running on port', config.server.port);
-  });
-
+  server.run(); // workers share the master socket
 }
